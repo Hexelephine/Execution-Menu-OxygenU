025fd36537cbdbfe0bf57adeda78435a81563d90
Hexelephine <126257261+Hexelephine@users.noreply.github.com>
2023-06-13 20:53:10 +0300

    Update execMenuMain.lua

--- execMenuMain.lua
+++ execMenuMain.lua
@@ -1,12 +1,1 @@
-local Line = Drawing.new("Line")
-Line.Visible = true
-Line.From = Vector2.new(0, 0)
-Line.To = Vector2.new(200, 200)
-Line.Color = Color3.fromRGB(255, 255, 255)
-Line.Thickness = 2
-Line.Transparency = 1
-LIne.ZIndex = 1
-
-wait(5)
-
-Line:Remove() --Drawing objects are manually managed.
+print("hello world")
